ee4ef8113994018ff2df80700ecc544e82c9c31e
yigitsuluova <yigitsulu432@gmail.com>
2024-03-16 21:05:40 +0300

    hayalet ve çocuk hareketi

---
+++ Assets/Scripts/ChildBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ChildBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _rightVelocity = 100f;
+     private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame){
+            transform.position += Vector3.right * _rightVelocity * Time.deltaTime;
+    }
+    }
+}
--- Assets/Scripts/FlyBehavior.cs
+++ Assets/Scripts/FlyBehavior.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem;
 public class FlyBehavior : MonoBehaviour
 {
     [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rightVelocity = 30000f;
     [SerializeField] private float _rotationSpeed = 10f;
     private Rigidbody2D rb2d;
     private void Start()
@@ -17,6 +18,9 @@ public class FlyBehavior : MonoBehaviour
     {
         if (Mouse.current.leftButton.wasPressedThisFrame){
             rb2d.velocity = Vector2.up * _velocity;
+            transform.position += Vector3.right * _rightVelocity * Time.deltaTime;
+
+            
         }
     }
     private void FixedUpdate()
