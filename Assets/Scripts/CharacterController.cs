989f9467dcebd7114a24cf1ecb945da9143d9e8a
yigitsuluova <yigitsulu432@gmail.com>
2024-03-16 03:27:09 +0300

    change1

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private float JumpForce = 10f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform charPos;
+    [SerializeField] private float groundDistance = 0.25f;
+    [SerializeField] private float JumpTime = 0.3f;
+
+    private bool isGrounded = false;
+    private bool isJumping = false;
+    private float JumpTimer;
+
+    private void Update(){
+        isGrounded = Physics2D.OverlapCircle(charPos.position, groundDistance, groundLayer);
+        if (isGrounded && Input.GetKeyDown("space")){
+            isJumping = true;
+            rb2d.velocity = Vector2.up * JumpForce;
+        }
+        if (isJumping && Input.GetKey("space")){
+            if (JumpTimer < JumpTime){
+                rb2d.velocity = Vector2.up * JumpForce;
+                JumpTimer += Time.deltaTime;
+            }else{
+                isJumping = false;
+            }
+            }
+            if (Input.GetKeyUp("space")){
+                isJumping = false;
+                JumpTimer = 0;
+            }
+        }
+        }
+    
+        
+
+
